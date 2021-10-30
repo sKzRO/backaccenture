3879e3df80715f647cc3263971f72d67fe9a6aa7
sKzRO <pinkkw0w@gmail.com>
2021-10-30 12:40:50 +0300

    mod

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
-// const firebase = require('firebase');
 var admin = require('firebase-admin');
 
 
@@ -38,6 +37,7 @@ MongoClient.connect(uri, { useUnifiedTopology: true,   useNewUrlParser: true})
     const locallaws = db.collection('locallaws');
     const localcustoms = db.collection('localcustoms');
     const events = db.collection('events');
+    const topics = db.collection('topics');
 
 
     app.use(bodyParser.urlencoded({ extended: true }));
@@ -49,6 +49,7 @@ MongoClient.connect(uri, { useUnifiedTopology: true,   useNewUrlParser: true})
     });
 
     // Rutele pentru mongo
+    // PLACES
     app.get('/getplaces', function (req, res) {
   
         places
@@ -62,8 +63,11 @@ MongoClient.connect(uri, { useUnifiedTopology: true,   useNewUrlParser: true})
 
       app.get('/getplace', function (req, res) {
   
+        console.log(req.query.id);
+        const id = req.query.id;
+
         places
-          .find({id: req.query.id})
+          .find({id: parseInt(id)})
           .toArray()
           .then((results) => {
             res.send(results);
@@ -72,7 +76,6 @@ MongoClient.connect(uri, { useUnifiedTopology: true,   useNewUrlParser: true})
       });
 
     app.post('/addplace', function (req, res) {
-
 
         places.countDocuments().then((currentId) => {
             const place = {
@@ -85,7 +88,8 @@ MongoClient.connect(uri, { useUnifiedTopology: true,   useNewUrlParser: true})
                 category: req.body.category,
                 address: req.body.address,
                 features: req.body.features,
-                website: req.body.website
+                website: req.body.website,
+                addedBy: req.body.uid
               };
     
               places.insertOne(place, (err, result) => {
@@ -94,21 +98,299 @@ MongoClient.connect(uri, { useUnifiedTopology: true,   useNewUrlParser: true})
               })
         });
   
+      });
 
+    //   RESTAURANTS
+      app.get('/getrestaurants', function (req, res) {
+  
+        restaurants
+          .find()
+          .toArray()
+          .then((results) => {
+            res.send(results);
+          })
+          .catch((error) => console.error(error));
+      });
 
+      app.get('/getrestaurant', function (req, res) {
+  
+        console.log(req.query.id);
+        const id = req.query.id;
+
+        restaurants
+          .find({id: parseInt(id)})
+          .toArray()
+          .then((results) => {
+            res.send(results);
+          })
+          .catch((error) => console.error(error));
+      });
+
+    app.post('/addrestaurant', function (req, res) {
+
+        restaurants.countDocuments().then((currentId) => {
+            const place = {
+                id: currentId+1,
+                title: req.body.title,
+                description: req.body.description,
+                rating: 0,
+                ratingNumbers: 0,
+                image: req.body.image,
+                category: req.body.category,
+                address: req.body.address,
+                features: req.body.features,
+                website: req.body.website,
+                addedBy: req.body.uid
+              };
+    
+              restaurants.insertOne(place, (err, result) => {
+                    console.log(result);
+                    res.send(result)
+              })
+        });
+  
       });
 
 
+      //   LOCAL CUSTOMS
+      app.get('/getlocalcustoms', function (req, res) {
+  
+        localcustoms
+          .find()
+          .toArray()
+          .then((results) => {
+            res.send(results);
+          })
+          .catch((error) => console.error(error));
+      });
+
+      app.get('/getlocalcustom', function (req, res) {
+  
+        console.log(req.query.id);
+        const id = req.query.id;
+
+        localcustoms
+          .find({id: parseInt(id)})
+          .toArray()
+          .then((results) => {
+            res.send(results);
+          })
+          .catch((error) => console.error(error));
+      });
+
+    app.post('/addlocalcustom', function (req, res) {
+
+        localcustoms.countDocuments().then((currentId) => {
+            const place = {
+                id: currentId+1,
+                title: req.body.title,
+                description: req.body.description,
+                rating: 0,
+                ratingNumbers: 0,
+                image: req.body.image,
+                category: req.body.category,
+                address: req.body.address,
+                features: req.body.features,
+                website: req.body.website,
+                addedBy: req.body.uid
+              };
+    
+              localcustoms.insertOne(place, (err, result) => {
+                    console.log(result);
+                    res.send(result)
+              })
+        });
+  
+      });
+
+
+            //   LOCAL CUSTOMS
+            app.get('/getlocallaws', function (req, res) {
+  
+                locallaws
+                  .find()
+                  .toArray()
+                  .then((results) => {
+                    res.send(results);
+                  })
+                  .catch((error) => console.error(error));
+              });
+        
+              app.get('/getlocallaw', function (req, res) {
+          
+                console.log(req.query.id);
+                const id = req.query.id;
+        
+                locallaws
+                  .find({id: parseInt(id)})
+                  .toArray()
+                  .then((results) => {
+                    res.send(results);
+                  })
+                  .catch((error) => console.error(error));
+              });
+        
+            app.post('/addlocallaw', function (req, res) {
+        
+                locallaws.countDocuments().then((currentId) => {
+                    const place = {
+                        id: currentId+1,
+                        title: req.body.title,
+                        description: req.body.description,
+                        article: req.body.article,
+                        year: req.body.year,
+                        category: req.body.category,
+                        addedBy: req.body.uid
+                      };
+            
+                      locallaws.insertOne(place, (err, result) => {
+                            console.log(result);
+                            res.send(result)
+                      })
+                });
+          
+              });
+
+
+            //   EVENTS
+            app.get('/getevents', function (req, res) {
+  
+                events
+                  .find()
+                  .toArray()
+                  .then((results) => {
+                    res.send(results);
+                  })
+                  .catch((error) => console.error(error));
+              });
+        
+              app.get('/getevent', function (req, res) {
+          
+                console.log(req.query.id);
+                const id = req.query.id;
+        
+                events
+                  .find({id: parseInt(id)})
+                  .toArray()
+                  .then((results) => {
+                    res.send(results);
+                  })
+                  .catch((error) => console.error(error));
+              });
+        
+            app.post('/addevent', function (req, res) {
+        
+                events.countDocuments().then((currentId) => {
+                    const place = {
+                        id: currentId+1,
+                        title: req.body.title,
+                        description: req.body.description,
+                        article: req.body.article,
+                        date: req.body.year,
+                        category: req.body.category,
+                        addedBy: req.body.uid
+                      };
+            
+                      events.insertOne(place, (err, result) => {
+                            console.log(result);
+                            res.send(result)
+                      })
+                });
+          
+              });
+
+            //   Topics
+            app.get('/getTopics', function (req, res) {
+  
+                topics
+                  .find()
+                  .toArray()
+                  .then((results) => {
+                    res.send(results);
+                  })
+                  .catch((error) => console.error(error));
+              });
+        
+              app.get('/getTopic', function (req, res) {
+          
+                console.log(req.query.id);
+                const id = req.query.id;
+        
+                topics
+                  .find({id: parseInt(id)})
+                  .toArray()
+                  .then((results) => {
+                    res.send(results);
+                  })
+                  .catch((error) => console.error(error));
+              });
+        
+            app.post('/addTopic', function (req, res) {
+        
+                topics.countDocuments().then((currentId) => {
+                    const place = {
+                        id: currentId+1,
+                        title: req.body.title,
+                        question: req.body.question,
+                        date: "2021",
+                        category: req.body.category,
+                        addedBy: req.body.uid,
+                        answears: []
+                      };
+            
+                      topics.insertOne(place, (err, result) => {
+                            console.log(result);
+                            res.send(result)
+                      })
+                });
+          
+              });
+
+              app.post('/addAnswear', function (req, res) {
+
+                const id = req.body.id;
+                const addedBy = req.body.uid;
+                const answear = req.body.answear;
+
+
+                topics
+                    .find({id: parseInt(id)})
+                    .toArray()
+                    .then((results) => {
+                        if (!results.answears) {
+                            const currentAnswear = {
+                                addedBy: addedBy,
+                                answear: answear
+                            }
+                            const vector = [];
+                            vector[0] = currentAnswear;
+                            topics.updateOne({
+                                id: id
+                            }, {
+                                $set: {
+                                    answears : vector
+                                }
+                            }).then(() => {
+                                res.send("gol")
+                            })
+                        } else {
+                            res.send(results.answears);
+                        }
+                    })
+                    .catch((error) => console.error(error));
+        
+          
+              });
+
     // Rutele pentru firebase
        // FIREBASE
-    //    var config = {
-    //     apiKey: process.env.APIKEY_FIREBASE,
-    //     authDomain: process.env.AUTHDOMAIN_FIREBASE,
-    //     databaseURL: process.env.DATABASEURL_FIREBASE,
-    //   };
+       var config = {
+        apiKey: process.env.APIKEY_FIREBASE,
+        authDomain: process.env.AUTHDOMAIN_FIREBASE,
+        databaseURL: process.env.DATABASEURL_FIREBASE,
+      };
     //   admin.initializeApp(config);
 
-    //     // Get a reference to the database service
-    //     var database = admin.database();
- 
+
+
 }).catch(console.error);
